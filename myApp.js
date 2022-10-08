e56c1b96b6fb7ed312f7a78b6e64bb228cfec5e2
mark-scholes <innominatus15@outlook.com>
2022-10-08 19:10:05 +0100

    This exercise requires us to return the index file with res.path()

--- myApp.js
+++ myApp.js
@@ -1,12 +1,15 @@
 let express = require('express');
 let app = express();
+const path = require('path')
 
 const Port = 3000; 
 
+const absolutePath = __dirname + '/views/index.html'
 
+console.log(absolutePath)
 
 app.get("/", function(req, res){
-    res.send("Hello Express")
+    res.sendFile(absolutePath)
 })
 
 
