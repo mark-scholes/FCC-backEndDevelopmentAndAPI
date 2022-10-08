c038ca6d5b0d4be23577e3149861fcfd9d1678e9
mark-scholes <innominatus15@outlook.com>
2022-10-08 21:07:50 +0100

    This exercise requires us alter the message style in the event a .env variable is present. Created .env and set up conditional formatting

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const dotenv = require('dotenv').config()
+
+const app = express();
+
+let PORT = process.env.PORT || 3000
 
 const absolutePath = __dirname + '/views/index.html'
 
@@ -11,8 +15,11 @@ app.get("/", function(req, res){
 
 // added a get request to a hard coded json object
 app.get("/json", function(req, res){
-    res.json({"message": "Hello json"})
+
+    process.env.MESSAGE_STYLE === "uppercase" ? 
+    res.json({"message": "Hello json".toUpperCase()}) : res.json({"message": "Hello json"})
 })
+
 
 app.use('/public', express.static('public'))
 
