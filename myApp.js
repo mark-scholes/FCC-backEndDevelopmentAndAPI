ca62e0a5b33a0b5059ba1a7f6fae91cb14af7b96
mark-scholes <innominatus15@outlook.com>
2022-10-10 20:03:05 +0100

    Get Route Parameter Input from the Client, in this challange we are getting params from the query string and echoing them back to the user

--- myApp.js
+++ myApp.js
@@ -5,45 +5,46 @@ const app = express();
 
 let PORT = process.env.PORT || 3000
 
-const absolutePath = __dirname + '/views/index.html'
+
+const homePath = __dirname + '/views/index.html'
 
 const getCurrentTime = () => {
     return new Date().toString()
 }
 
-
-
+//logger logs request details: method, path and ip
 app.use((req, res, next) => {
-
     let string = `${req.method} ${req.path} - ${req.ip}`
-    console.log(string) 
-      
-     next();
-   
+    console.log(string);      
+     next();   
 });
 
+//returns html for home route
+app.get("/", (req, res, ) =>{    
+    res.sendFile(homePath)
+});
 
-
-
-app.get("/", function(req, res, ){
-    
-    res.sendFile(absolutePath)
-})
-
-// added a get request to a hard coded json object
-app.get("/json", function(req, res){
-
+// hard coded .env variable returns conditionally formated json
+app.get("/json", (req, res) => {
     process.env.MESSAGE_STYLE === "uppercase" ? 
-    res.json({"message": "Hello json".toUpperCase()}) : res.json({"message": "Hello json"})
+    res.json({"message": "the meaning of life the universe and everything".toUpperCase()}) : res.json({"message": "The meaning of life the universe and everything"})
 });
 
-app.get('/now', function (req,res, next){
+// Route for /now which adds a key to the request object of .time. This calls a function to get current time. Once returned send as json   
+app.get('/now', (req,res, next) =>{
     req.time = getCurrentTime();
     next();
-}, function(req, res) {
+}, (req, res) => {
     res.send({time: req.time})
-}
-)
+});
+
+// gets query string params and echos them back as json  
+app.get('/:word/echo', (req, res) => {
+    const {word} = req.params;
+    res.json({
+        echo: word
+    });    
+});
 
 
 
